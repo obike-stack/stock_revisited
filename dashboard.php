a78034607c0ec08b5e24d615e5787230005ffe6d
obike-stac <obikeimmanuel@gmail.com>
2019-10-15 15:34:13 +0300

    This is the first commit am making while using the hackable editor Atom the great

---
+++ dashboard.php
@@ -0,0 +1,197 @@
+<?php require_once 'includes/header.php'; ?>
+
+<?php 
+
+$sql = "SELECT * FROM product WHERE status = 1";
+$query = $connect->query($sql);
+$countProduct = $query->num_rows;
+
+$orderSql = "SELECT * FROM orders WHERE order_status = 1";
+$orderQuery = $connect->query($orderSql);
+$countOrder = $orderQuery->num_rows;
+
+$totalRevenue = 0;
+while ($orderResult = $orderQuery->fetch_assoc()) {
+	
+	$totalRevenue += @$orderResult['paid'];
+}
+
+$lowStockSql = "SELECT * FROM product WHERE quantity <= 3 AND status = 1";
+$lowStockQuery = $connect->query($lowStockSql);
+$countLowStock = $lowStockQuery->num_rows;
+
+$dataPoints = array();
+$stockSql = "SELECT * FROM product";
+$chartQuery = $connect->query($stockSql);
+while ($row = $chartQuery->fetch_assoc()) {
+	array_push($dataPoints,array("y" => $row['quantity']  , "label" => $row['product_name']));
+}
+
+
+$connect->close();
+
+
+?>
+
+
+<style type="text/css">
+	.ui-datepicker-calendar {
+		display: none;
+	}
+</style>
+
+<!-- fullCalendar 2.2.5-->
+    <link rel="stylesheet" href="assests/plugins/fullcalendar/fullcalendar.min.css">
+    <link rel="stylesheet" href="assests/plugins/fullcalendar/fullcalendar.print.css" media="print">
+
+
+<div class="row">
+	
+	<div class="col-md-4">
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				
+				<a href="product.php" style="text-decoration:none;color:black;">
+					Total Product
+					<span class="badge pull pull-right"><?php echo $countProduct; ?></span>	
+				</a>
+				
+			</div> <!--/panel-hdeaing-->
+		</div> <!--/panel-->
+	</div> <!--/col-md-4-->
+
+	<div class="col-md-4">
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<a href="orders.php?o=manord" style="text-decoration:none;color:black;">
+					Total Orders
+					<span class="badge pull pull-right"><?php echo $countOrder; ?></span>
+				</a>
+					
+			</div> <!--/panel-hdeaing-->
+		</div> <!--/panel-->
+	</div> <!--/col-md-4-->
+
+	<div class="col-md-4"  >
+		<div class="panel panel-danger">
+			<div class="panel-heading">
+				<a href="product.php" style="text-decoration:none;color:black;">
+					Low Stock
+					<span class="badge pull pull-right"><?php echo $countLowStock; ?></span>	
+				</a>
+				
+			</div> <!--/panel-hdeaing-->
+		</div> <!--/panel-->
+	</div> <!--/col-md-4-->
+
+	<div class="col-md-4">
+		<div class="card">
+		  <div class="cardHeader">
+		    <h1><?php echo date('d'); ?></h1>
+		  </div>
+
+		  <div class="cardContainer">
+		    <p><?php echo date('l') .' '.date('d').', '.date('Y'); ?></p>
+		  </div>
+		</div> 
+		<br/>
+
+		<div class="card" >
+		  <div class="cardHeader" style="background-color:#245580;">
+		    <h1><?php if($totalRevenue) {
+		    	echo $totalRevenue;
+		    	} else {
+		    		echo '0';
+		    		} ?></h1>
+		  </div>
+
+		  <div class="cardContainer">
+		    <p> <i class="glyphicon glyphicon-ksh"></i> Total Revenue</p>
+		  </div>
+		</div> 
+	</div>
+
+		<div class="col-md-8">
+	        
+
+	        <div class="">
+	        	<div id="graphical" class="container m-auto  p-2" style="height: 330px; width: 100%;">
+	                <div id="chartContainer" style="height: 300px; width: 100%;">
+	                </div>
+	            </div>
+	            </div>
+	    </div>
+
+
+</div> <!--/row-->
+
+	<div style="width: 30%" >
+
+		<div class="panel panel-default">
+			<div class="panel-heading"> <i class="glyphicon glyphicon-calendar"></i> CALENDAR</div>
+			<div class="panel-body">
+				<div id="calendar"></div>
+			</div>	
+		</div>
+	</div>
+<!-- fullCalendar 2.2.5 -->
+<script src="assests/plugins/moment/moment.min.js"></script>
+<script src="assests/plugins/fullcalendar/fullcalendar.min.js"></script>
+
+
+<script type="text/javascript">
+	$(function () {
+			// top bar active
+	$('#navDashboard').addClass('active');
+
+      //Date for the calendar events (dummy data)
+      var date = new Date();
+      var d = date.getDate(),
+      m = date.getMonth(),
+      y = date.getFullYear();
+
+      $('#calendar').fullCalendar({
+        header: {
+          left: '',
+          center: 'title'
+        },
+        buttonText: {
+          today: 'today',
+          month: 'month'          
+        }        
+      });
+
+
+    });
+</script>
+
+<?php require_once 'includes/footer.php'; ?>
+
+
+<script>
+
+    
+        
+function loadChart() {
+      
+      var chart = new CanvasJS.Chart("chartContainer", {
+	      animationEnabled: true,
+        title: {
+          text: "STOCK SUMMARY"
+        },
+        axisY: {
+          title: "STOCK"
+        },
+        data: [{
+          markerType: "circle",
+          type: "bar",
+          dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+        }]
+      });
+      console.log(chart);
+      chart.render();
+       
+      }
+      
+    loadChart();
+</script>
